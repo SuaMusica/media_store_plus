1157691627859dde5e328f85cc03683ccb81938b
Felipe Sales <soufeliposales@gmail.com>
2024-06-28 19:03:10 -0300

    build: removed import unnecessary

--- android/src/main/kotlin/com/snnafi/media_store_plus/MediaStorePlusPlugin.kt
+++ android/src/main/kotlin/com/snnafi/media_store_plus/MediaStorePlusPlugin.kt
@@ -28,7 +28,6 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
 import java.io.File
-import java.nio.file.Files
 import java.io.FileOutputStream
 import java.util.*
 
