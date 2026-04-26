bbf632353af05245cf750820db92ca7709bc0af0
Alan Trope <atrope@gmail.com>
2026-04-26 19:50:17 -0300

    fix: correct syntax for cursor query in MediaStorePlusPlugin
    - Fixed closing parentheses in cursor query methods to ensure proper execution.
    - Improved code readability and maintainability by aligning method call syntax.

--- android/src/main/kotlin/com/snnafi/media_store_plus/MediaStorePlusPlugin.kt
+++ android/src/main/kotlin/com/snnafi/media_store_plus/MediaStorePlusPlugin.kt
@@ -589,7 +589,7 @@ class MediaStorePlusPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
             MediaStore.Audio.Media.DISPLAY_NAME + " =?  AND " + MediaStore.Audio.Media.RELATIVE_PATH + " =? ",
             selectionArgs,
             null
-        }
+        )
         Log.d(TAG, "getUriFromDisplayName: $uri")
         return cursor?.use {
             if (it.moveToFirst()) {
@@ -775,7 +775,7 @@ class MediaStorePlusPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
             null,
             null,
             null
-        }
+        )
 
         return cursor?.use {
             if (it.moveToFirst()) {
